47fa65390f524dfed6bec473139913ba960c744e
ZayraGS1403 <142755654+ZayraGS1403@users.noreply.github.com>
2023-09-06 22:27:51 -0500

    Update Lib2matrces .py
    se añadieron 7 librerias 9-15

--- Lib2matrces.py
+++ Lib2matrces.py
@@ -1,3 +1,6 @@
+import math
+import numpy
+
 def sumavec(v1, v2):
     c=[]
     if len(v1) == len(v2):
@@ -67,3 +70,59 @@ def conjugada(m1):
         for j in range(colum):
             m1[i][j] = m1[i][j].conjugate()
     return m1
+
+def adjunta(m1):
+    m1 = transpuesta(m1)
+    m1 = conjugada(m1)
+    return m1
+
+def prod_de_matz(m1, m2):
+    fl_a = len(m1)
+    cl_a = len(m1[0])
+    fl_b = len(m2)
+    cl_b = len(m2[0])
+    if cl_a == fl_b:
+        matriz = []
+        for i in range(cl_b):
+            fila = []
+            for i in range(fl_a):
+                sum = 0
+                for j in range(cl_a):
+                    sum += m1[i][j] * m2[j][i]
+                fila.append(sum)
+            matriz.append(fila)
+    else:
+        matriz = "Tamaño errado"
+    return matriz
+
+def prod_interno(v1, v2):
+    if len(v1) == len(v2):
+        sum = 0
+        for i in range(len(v1)):
+            sum  += v1[i].conjugate()*v2[i]
+    else: sum = "Tamaño errado"
+    return sum
+
+def vectornormal(v1):
+    b = prod_interno(v1, v1)
+    b = round(math.sqrt(b.real),2)
+    return b
+
+def distancia(v1, v2):
+    h = sumavec(v1, inverso(v2))
+    dis = vectornormal(h)
+    return dis
+
+def vectores_prop(m1):
+    m1 = numpy.asarray(m1)
+    tam = m1.shape
+    fil = tam[0]
+    col = tam[1]
+    if fil == col:
+        val , vecto = numpy.linalg.eig(m1)
+        vecto = numpy.transpose(vecto)
+        valor = numpy.round(val,2)
+        vector = numpy.round(vecto, 2)
+        return  "valores: {}. vectores: {}".format(valor, vecto[0])
+    else:
+        return "Intoduzca una matrz cuadrada"
